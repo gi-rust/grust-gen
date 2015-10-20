af16d86259e83b0696fa36446558c7d846f9775a
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-10-20 10:03:59 +0300

    Eliminate the lib subdirectory
    It has only one package, and the source tree has become a bamboo pole.

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ setup(
     name='grust-gen',
     setup_requires=['setuptools_scm'],
     use_scm_version={
-        'write_to': 'lib/grust/version.py'
+        'write_to': 'grust/version.py'
     },
     packages=['grust'],
-    package_dir={
-        'grust': 'lib/grust'
-    },
     package_data={
         'grust': ['templates']
     },
