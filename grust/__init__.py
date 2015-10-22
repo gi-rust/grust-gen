886f6426c582f7a0821b750e7ad78811444d4d05
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-10-22 08:31:07 +0300

    grust: Append a dirty tag to __version__ if running in dirty git worktree
    I want clear indication in the generator version stamp when generating
    files using a dirty working tree of grust-gen. setuptools_scm only
    updates the version when setup.py is run.

--- grust/__init__.py
+++ grust/__init__.py
@@ -1,5 +1,28 @@
-try:
-    from .version import version as __version__
-except ImportError:
-    # The package has not been installed; try setup.py install or develop
-    __version__ = None
+def _get_version():
+    try:
+        from .version import version
+    except ImportError:
+        # The package has not been installed; try setup.py install or develop
+        return None
+
+    import os
+
+    parent_dir = os.path.join(os.path.dirname(__file__), '..')
+    if os.path.isdir(os.path.join(parent_dir, '.git')):
+
+        import subprocess
+
+        try:
+            out = subprocess.check_output(['git', 'status', '--porcelain'],
+                                          cwd=parent_dir)
+        except Exception:
+            out = ''
+        if len(out) > 0:
+            if '+' in version:
+                version += '.dirty'
+            else:
+                version += '+dirty'
+
+    return version
+
+__version__ = _get_version()
