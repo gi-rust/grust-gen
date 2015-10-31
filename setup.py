fa53c2702f809fb293312afc3b2dac1fc73a3d17
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-10-31 15:14:13 +0200

    setup.py: Added classifiers metadata

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ setup(
     url='https://github.com/gi-rust/grust-gen',
     author='Mikhail Zabaluev',
     author_email='mikhail.zabaluev@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Rust',
+        'Topic :: Software Development :: Code Generators',
+    ],
     setup_requires=['setuptools_scm'],
     use_scm_version={
         'write_to': 'grust/version.py'
