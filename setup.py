29ae0e968d4c1f0e7d42928214196caa339e5674
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-11-04 08:00:31 +0200

    setup.py: advertise Python 3 support in the package metadata

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Rust',
         'Topic :: Software Development :: Code Generators',
     ],
